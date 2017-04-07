bb503e3bcb61a624f4b9187b3a29cb6f6263d061
cqoyq <cqoyq@163.com>
2017-04-07 17:01:32 +0800

    v1.4.0

--- bingo/TCP/pack_and_unpack/pack_handler.h
+++ bingo/TCP/pack_and_unpack/pack_handler.h
@@ -16,21 +16,27 @@ using namespace std;
 #include "bingo/configuration/node.h"
 #include "net_layer.h"
 
-namespace bingo { namespace TCP { namespace pack_and_unpack {
+namespace bingo {
+          namespace TCP {
+                    namespace pack_and_unpack {
+                              
+                              template<typename T>
+                              class pack_handler {
+                              public:
 
-class pack_handler
-{
-public:
-	virtual ~pack_handler(){}
-   /* Pack data to stream.
-    * return 0 if success, otherwise return -1. */
-   virtual int handler(bingo::configuration::node*& in_data, net_layer*& in_net, char*& out_data, size_t& out_data_size)=0;
+                                        virtual ~pack_handler() {
+                                        }
+                                        /* Pack data to stream.
+                                         * return 0 if success, otherwise return -1. */
+                                        virtual int handler(bingo::configuration::node*& in_data, T*& in_net, char*& out_data, size_t& out_data_size) = 0;
 
-protected:
-private:
+                              protected:
+                              private:
 
-};
+                              };
 
-}  }  }
+                    }
+          }
+}
 
 #endif /* BINGO_TCP_PACK_AND_UNPACK_PACK_HANDLER_H_ */
--- bingo/TCP/pack_and_unpack/unpack_handler.h
+++ bingo/TCP/pack_and_unpack/unpack_handler.h
@@ -16,21 +16,27 @@ using namespace std;
 #include "bingo/configuration/node.h"
 #include "net_layer.h"
 
-namespace bingo { namespace TCP { namespace pack_and_unpack {
+namespace bingo {
+          namespace TCP {
+                    namespace pack_and_unpack {
+                              
+                               template<typename T>
+                              class unpack_handler {
+                              public:
 
-class unpack_handler
-{
-public:
-	virtual ~unpack_handler(){};
-   /* Unpack stream to data.
-    * return 0 if success, otherwise return -1. */
-   virtual int handler(char*& in_data, size_t& in_data_size, net_layer*& in_net,  bingo::configuration::node*& out_data)=0;
+                                        virtual ~unpack_handler() {
+                                        };
+                                        /* Unpack stream to data.
+                                         * return 0 if success, otherwise return -1. */
+                                        virtual int handler(char*& in_data, size_t& in_data_size, T*& in_net, bingo::configuration::node*& out_data) = 0;
 
-protected:
-private:
+                              protected:
+                              private:
 
-};
+                              };
 
-}  }  }  /* BINGO_TCP_PACK_AND_UNPACK_UNPACK_HANDLER_H_ */
+                    }
+          }
+} /* BINGO_TCP_PACK_AND_UNPACK_UNPACK_HANDLER_H_ */
 
 #endif
